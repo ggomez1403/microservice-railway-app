cf2a3d0def7391d738298b2effe28818a7c40cbe
Geraldine Gómez <geralgomez1403@gmail.com>
2023-12-08 00:11:14 -0500

    initial commit

---
+++ src/main/java/com/ggomezr/microservicerailwayapp/MicroserviceRailwayAppApplication.java
@@ -0,0 +1,13 @@
+package com.ggomezr.microservicerailwayapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceRailwayAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceRailwayAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ggomezr/microservicerailwayapp/MicroserviceRailwayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggomezr.microservicerailwayapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceRailwayAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
